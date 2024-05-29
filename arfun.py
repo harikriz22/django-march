78943849d5965e4324b22d6367c8983880a31c14
Harikrishnan <54831389+harikriz22@users.noreply.github.com>
2024-05-29 19:55:58 +0530

    program for something

---
+++ arfun.py
@@ -0,0 +1,38 @@
+# # calculator using function only
+# #age calculator
+# #bmi calculator
+# # weight/(height in meter)**2
+
+# a=int(input('enter first'))
+# b=int(input('enter second'))
+
+# c=input('add sub mul div')
+# def add(a,b):
+#     return a+b
+# def sub(a,b):
+#     return(a-b)
+# def mul(a,b):
+#     return(a*b)
+# def div(a,b):
+#     return(a/b)
+
+# if c=='add':
+#     print(add(a,b))
+# elif c=='sub':
+#     print(sub(a,b))  
+# elif c=='mul':
+#     print(mul(a,b))
+# else:
+#     print(div(a,b))
+
+
+# def getvalue(a):
+#     return a*10
+
+# z= getvalue(10) *10
+
+# print(z)
+
+
+
+
